3992e8e152d8e50542041d5b073c146a90e758eb
drexel-ue <packappsforyou@gmail.com>
2020-07-09 07:52:45 -0400

    Add basic page view for home screen

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,13 +1,83 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:skype_clone/utils/skype_colors.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  PageController _pageController;
+  int _page = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 1);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home'),
+      backgroundColor: SkypeColors.blackColor,
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (int page) => setState(() => _page = page),
+        children: [
+          Center(child: Text('Chat List Screen')),
+          Center(child: Text('Call Logs')),
+          Center(child: Text('Contact Screen')),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: CupertinoTabBar(
+          backgroundColor: SkypeColors.blackColor,
+          items: [
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  _pageController.animateToPage(0, duration: const Duration(milliseconds: 250), curve: Curves.easeOut);
+                },
+                child: Icon(
+                  Icons.chat_bubble_outline,
+                  color: _page == 0 ? SkypeColors.lightBlueColor : SkypeColors.greyColor,
+                ),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  _pageController.animateToPage(1, duration: const Duration(milliseconds: 250), curve: Curves.easeOut);
+                },
+                child: Icon(
+                  Icons.phone,
+                  color: _page == 1 ? SkypeColors.lightBlueColor : SkypeColors.greyColor,
+                ),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  _pageController.animateToPage(2, duration: const Duration(milliseconds: 250), curve: Curves.easeOut);
+                },
+                child: Icon(
+                  Icons.contacts,
+                  color: _page == 2 ? SkypeColors.lightBlueColor : SkypeColors.greyColor,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
